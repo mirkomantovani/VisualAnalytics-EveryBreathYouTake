8265ef4af85b4554055c9a00e62dcfda86419cdf
Ashwani Khemani <ashwani.khemani@gmail.com>
2019-02-20 11:23:31 -0600

    Changes done to get the data files from the website

---
+++ dataRead.R
@@ -0,0 +1,29 @@
+# SCript to get all the data for the project from the EPA website
+
+library(rvest)
+
+pg <- read_html("https://aqs.epa.gov/aqsweb/airdata/download_files.html")
+links <- data.frame(url = html_attr(html_nodes(pg, "a"), "href"))
+d_urls <- data.frame(file_name = links[grep("daily_aqi_by_county", links$url),])
+d_urls <- rbind(d_urls,data.frame(file_name  = links[grep("hourly_WIND", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_TEMP", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_44201", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_42401", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_42101", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_42602", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_88101", links$url),]))
+d_urls <- rbind(d_urls,data.frame(file_name = links[grep("hourly_81102", links$url),]))
+
+
+d_urls$url_name <- paste("https://aqs.epa.gov/aqsweb/airdata/", d_urls$file_name,sep="")
+
+workingdir <-getwd()
+
+for (row in 1:nrow(d_urls)) {
+  link <- d_urls[row,"url_name"]
+  download.file(link,file.path("data",basename(link)), method = "libcurl", quiet = TRUE)
+  }
+
+# unzip(basename(link), files = NULL, list = FALSE, overwrite = TRUE,
+#       junkpaths = FALSE, exdir = ".", unzip = "internal",
+#       setTimes = FALSE)
